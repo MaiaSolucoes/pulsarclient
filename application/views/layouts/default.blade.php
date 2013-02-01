45d181cb4e3113e767733869b73c63606791dda8
cicero@maiasolucoes.com.br <cicerocasj@hotmail.com>
2013-02-01 14:10:14 -0200

    atualizacao tag head

--- application/views/layouts/default.blade.php
+++ application/views/layouts/default.blade.php
@@ -6,12 +6,6 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link href="../assets/css/bootstrap.css" rel="stylesheet">
-    <style type="text/css">
-            /*body {
-                padding-top: 60px;
-                padding-bottom: 40px;
-            }*/
-    </style>
     <link href="../assets/css/bootstrap-responsive.css" rel="stylesheet">
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
@@ -23,7 +17,6 @@
     <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
     <link rel="shortcut icon" href="favicon.ico">
     <link href='http://fonts.googleapis.com/css?family=Crete+Round' rel='stylesheet' type='text/css'>
-    {{ Asset::styles(); }}
     {{ Asset::container('bootstrapper')->styles(); }}
 </head>
 <body>
