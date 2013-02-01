40dbc42d8a93577b5011b6d1c2f3d4aece688f92
raphael <raphael.wcosta@gmail.com>
2013-02-01 10:49:44 -0200

    removido arquivos css sem utilidade

--- application/views/layouts/default.blade.php
+++ application/views/layouts/default.blade.php
@@ -14,18 +14,10 @@
         }
     </style>
     <link href="../assets/css/bootstrap-responsive.css" rel="stylesheet">
-
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-
-    <!-- Fav and touch icons -->
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
     <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
-    <link rel="shortcut icon" href="favicon.ico">
     <LINK rel="stylesheet" type="text/css" href="public/bundles/css/screen.css">
     <link href='http://fonts.googleapis.com/css?family=Crete+Round' rel='stylesheet' type='text/css'>
     {{ Asset::styles(); }}
