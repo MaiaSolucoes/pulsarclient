d02dcaad802eb401467be024b8d14c18da92b070
raphael <raphael.wcosta@gmail.com>
2013-03-11 20:30:43 -0300

    criado branch 1 e adicionado metodo prepare para reduzir o codigo

--- Pulsar/pulsar.php
+++ Pulsar/pulsar.php
@@ -1,44 +1,46 @@
 <?php
 
-    class Pulsar {
+class Pulsar {
 
-        public static function login ($arguments = array()){
-            if (empty($arguments['username']) or empty($arguments['password'])){
-                return "Usuario ou senha em branco";
-            }
-            $url = "http://pul.cicero.maiasolucoes.com.br/auth/login?username=";
-            $url .= "$arguments[username]&password=$arguments[password]";
-            $status = self::status($url);
-            switch($status){
-                case "200":
-                    $content = file_get_contents($url);
-                    $token = json_decode($content);
-                    break;
-                case "404":
-                    $token = "Not found";
-                    break;
-                default:
-                    $token = "Error";
-            }
-            return $token;
-        }
-
-        public static function who($token){
-            $url = "http://pul.cicero.maiasolucoes.com.br/user/user?token=$token";
-            $content = file_get_contents($url);
-            $data = json_decode($content);
-            return $data;
-        }
-
-        public static function logout(){
-            $content = file_get_contents("http://pul.cicero.maiasolucoes.com.br/auth/logout");
-            $data = json_decode($content);
-            return $content;
-        }
-
-        public static function status($url) {
-            // RETURN HTTP STATUS CODE
-            $headers = get_headers($url);
-            return substr($headers[0], 9, 3);
-        }
+    public static function prepare($url) {
+        $content = file_get_contents($url);
+        $json = json_decode($content);
+        return $json;
     }
+    public static function login ($arguments = array()){
+        if (empty($arguments['username']) or empty($arguments['password'])){
+            return "Empty Fields";
+        }
+        $url = "http://pul.cicero.maiasolucoes.com.br/auth/login?username=";
+        $url .= "$arguments[username]&password=$arguments[password]";
+        $status = self::status($url);
+        switch($status){
+            case "200":
+                $data = self::prepare($url);
+                break;
+            case "404":
+                $data = "Not found";
+                break;
+            default:
+                $data = "Error";
+        }
+        return $data;
+    }
+
+    public static function who($token){
+        $url = "http://pul.cicero.maiasolucoes.com.br/user/user?token=$token";
+        $data = self::prepare($url);
+        return $data;
+    }
+
+    public static function logout(){
+        $content = self::prepare("http://pul.cicero.maiasolucoes.com.br/auth/logout");
+        return $content;
+    }
+
+    public static function status($url) {
+        // RETURN HTTP STATUS CODE
+        $headers = get_headers($url);
+        return substr($headers[0], 9, 3);
+    }
+}
--- index.php
+++ index.php
@@ -2,4 +2,9 @@
 require_once 'Pulsar/pulsar.php';
 $args = array('username' => 'raphael@maiasolucoes.com.br', 'password' => 'raphael');
 $token = Pulsar::login($args);
-print Pulsar::who($token);
+print "$token<br>";
+$t = "http://pul.cicero.maiasolucoes.com.br/user/user?token=$token";
+$d = file_get_contents($t);
+$de = json_decode($d);
+print_r($de);
+//print_r(Pulsar::who($token));
